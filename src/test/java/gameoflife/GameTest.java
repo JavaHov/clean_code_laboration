51a8d38ef4c2556bf1b2401d5bc716eabfc9a2df
Ola Lindgård <ola.lindgard@iths.se>
2021-12-16 09:44:29 +0100

    Initiated project with test file and game file.

---
+++ src/test/java/gameoflife/GameTest.java
@@ -0,0 +1,12 @@
+package gameoflife;
+
+import org.junit.jupiter.api.Test;
+
+public class GameTest {
+
+    @Test
+    public void oneLivingNeighbour() {
+        Game game = new Game();
+    }
+
+}
