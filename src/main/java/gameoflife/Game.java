ab0167ca7fd5e88d2bb9eb4f233399e1cb8510ef
Ola Lindgård <ola.lindgard@iths.se>
2021-12-18 16:24:34 +0100

    Forgot to commit... A couple of working tests.

--- src/main/java/gameoflife/Game.java
+++ src/main/java/gameoflife/Game.java
@@ -2,9 +2,71 @@ package gameoflife;
 
 public class Game {
 
+    private final int rows;
+    private final int columns;
+    private final static int DEAD = 0;
+    private final static int ALIVE = 1;
 
-    public int getLivingNeighbours() {
+    private final int[][] matrix;
 
-        return 1;
+    public Game(int rows, int columns) {
+        matrix = new int[rows][columns];
+        this.rows = rows;
+        this.columns = columns;
+        initializeCleanMatrix();
+    }
+
+    private void initializeCleanMatrix() {
+
+        for(int y = 0; y < rows; y++) {
+            for(int x = 0; x < columns; x++) {
+                matrix[y][x] = DEAD;
+                System.out.print(matrix[y][x]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+    }
+
+    public int getLivingNeighbours(int row, int column) {
+        int[][] areaToCheck = {{row-1, column-1},
+                                {row-1, column},
+                                {row-1, column+1},
+                                {row, column-1},
+                                {row, column+1},
+                                {row+1, column-1},
+                                {row+1, column},
+                                {row+1, column+1}};
+
+        int livingNeighbours = 0;
+
+        for(int i = 0; i < areaToCheck.length; i++) {
+
+            int rowToCheck = areaToCheck[i][0];
+            int columnToCheck = areaToCheck[i][1];
+
+            if(withinMatrix(rowToCheck, columnToCheck) && isAlive(rowToCheck, columnToCheck)){
+                livingNeighbours++;
+            }
+        }
+        return livingNeighbours;
+    }
+
+    private boolean isAlive(int row, int column) {
+        return matrix[row][column] == ALIVE;
+    }
+
+    private boolean isDead(int row, int column) {
+        return matrix[row][column] == DEAD;
+    }
+
+    private boolean withinMatrix(int row, int column) {
+        return row >= 0 && row < this.rows && column >= 0 && column < this.columns;
+    }
+
+    public void setLivingCell(int y, int x) {
+        matrix[y][x] = ALIVE;
     }
 }
--- src/test/java/gameoflife/GameTest.java
+++ src/test/java/gameoflife/GameTest.java
@@ -1,17 +1,50 @@
 package gameoflife;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class GameTest {
 
+
+    private Game game = new Game(8, 8);
+
+
+
+
+    @Test
+    public void zeroLivingNeighbours() {
+        int numNeighbours = game.getLivingNeighbours(2, 4);
+        assertThat(numNeighbours).isEqualTo(0);
+
+    }
+
+    @Test
+    public void zeroLivingNeighboursAdjacent() {
+
+        game.setLivingCell(0,0);
+        game.setLivingCell(0,4);
+        game.setLivingCell(4,0);
+        game.setLivingCell(4,4);
+        int numNeighbours = game.getLivingNeighbours(2,2);
+        assertThat(numNeighbours).isEqualTo(0);
+    }
+
     @Test
     public void oneLivingNeighbour() {
-        Game game = new Game();
-        int numNeighbours = game.getLivingNeighbours();
+
+        game.setLivingCell(3, 4);
+        int numNeighbours = game.getLivingNeighbours(3, 5);
         assertThat(numNeighbours).isEqualTo(1);
+    }
 
+    @Test
+    public void twoLivingNeighbours() {
 
+        game.setLivingCell(1,4);
+        game.setLivingCell(1,5);
+        int numNeighbours = game.getLivingNeighbours(0,4);
+        assertThat(numNeighbours).isEqualTo(2);
     }
 
 }
